259658f4f92cd653c03278dae901c7c656ef624e
malachieborohoul <malachieborohoul@gmail.com>
2024-08-13 04:19:02 +0100

    Modifier variable d'env

--- api/db.js
+++ api/db.js
@@ -1,5 +1,4 @@
 const Pool = require("pg").Pool;
-require('dotenv').config();
 const pool = new Pool({
     user: process.env.PG_USER,
     // url: "postgres://uvlqydnyxdwlic:9799d89690fb279f9a0d1418b83a3f6d7d887fdb35ce7d82106b8fc1fc2684d1@ec2-44-205-177-160.compute-1.amazonaws.com:5432/d68rjs1nalccnl",

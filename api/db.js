5907d0a64275a44ff40737d7abbe1ca253d88ee2
malachieborohoul <malachieborohoul@gmail.com>
2024-08-13 03:50:05 +0100

    Change db configurations

--- api/db.js
+++ api/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
-
+const connectionString = 'postgresql://postgres.rucakomgxanrjakyhrxs:[YOUR-PASSWORD]@aws-0-us-east-1.pooler.supabase.com:6543/postgres'
 const pool = new Pool({
-    user: "postgres",
+    user: "postgres.rucakomgxanrjakyhrxs",
     // url: "postgres://uvlqydnyxdwlic:9799d89690fb279f9a0d1418b83a3f6d7d887fdb35ce7d82106b8fc1fc2684d1@ec2-44-205-177-160.compute-1.amazonaws.com:5432/d68rjs1nalccnl",
-    host: "db.rucakomgxanrjakyhrxs.supabase.co",
+    host: "aws-0-us-east-1.pooler.supabase.com",
     database: "postgres",
     password: "9@Q5j@VEGh7mbgt",
-    port: 5432
+    port: 6543
 });
 module.exports = pool;

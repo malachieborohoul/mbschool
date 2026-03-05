3fe54d95d035d6a44b23c3cdbd19e085defff2f4
malachieborohoul <malachieborohoul@gmail.com>
2026-03-05 05:43:28 +0100

    * Roll back to old siignature

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "app.bsm.mbschool"
+    namespace = "com.bsm.mbschool"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "app.bsm.mbschool"
+        applicationId = "com.bsm.mbschool"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
